81d46f6cdf7a6604c10b5493dada6cae9f876217
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-07-17 17:49:12 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 
-
 ReactDOM.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
-  document.getElementById('root'));
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
